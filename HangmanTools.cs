53f82760a587fd5b05ba58587c4962f10229c5bb
Da111an <dallangrow@gmail.com>
2026-01-28 03:11:44 -0700

    added more stuff to HangmanTools.cs

--- HangmanTools.cs
+++ HangmanTools.cs
@@ -21,7 +21,7 @@ namespace HangmanStuff
                 "Net",
                 "Demon"
             };
-
+            // Get a random word.
             word = words[r.Next(words.Count)];
 
             return word;
@@ -31,7 +31,45 @@ namespace HangmanStuff
 
         public bool ValidGuess(string guess, string lettersGuessed)
         {
+            bool result = true; //Default to valid guess
 
+            if (guess.Length != 1) //
+            {
+                Console.WriteLine("Sorry, the guess can only be one letter.");
+                result = false;
+            }
+
+            else if (!Char.IsLetter(guess[0]))
+            {
+                Console.WriteLine("Sorry, the guess needs to be a letter.");
+                result = false;
+            }
+
+            else if (lettersGuessed.Contains(guess))
+            {
+                Console.WriteLine("Sorry, you have already guessed the letter " + guess);
+                result = false;
+            }
+            return result;
+        }
+
+        public string UpdateWord(string lettersGuessed, string solution)
+        {
+            string result = "";
+
+            for (int i = 0; i < solution.Length; i++)
+            {
+                if (lettersGuessed.Contains(solution[i]))
+                {
+                    result += solution[i];
+                }
+                else
+                {
+                    result += "_";
+                }
+            }
+
+            return result;
         }
     }
 }
